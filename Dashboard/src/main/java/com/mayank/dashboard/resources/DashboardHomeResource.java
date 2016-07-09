cb5e609a0a13a4b25336e838d53d95d3bab1f35d
mayankWorks <mayank.shah88@gmail.com>
2016-07-09 14:23:27 +0530

    Dashboard first commit

---
+++ Dashboard/src/main/java/com/mayank/dashboard/resources/DashboardHomeResource.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.mayank.dashboard.resources;
+
+/**
+ * @author Maverick
+ *
+ */
+public class DashboardHomeResource {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Your main method is running");
+	}
+
+}
